3d4113e917445c1835ff84b544fcb3e52ccab765
unknown <laondohemba@gmail.com>
2025-01-05 23:57:29 +0100

    changed examiner dashboard interface

--- resources/views/components/examinerdashboard.blade.php
+++ resources/views/components/examinerdashboard.blade.php
@@ -1,37 +1,22 @@
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Examinations</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="{{route('examination.create')}}">Create examination</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
+<div class="row my-5">
+  <div class="col-3 text-center">
+    <div class="d-flex flex-column">
+      <a href="{{route('examiner.dashboard')}}" class="text-decoration-none">
+        <i class="fa fa-book" style="font-size:4rem; color:black"></i>
+      
+      <h4>Examinations</h4>
+    </a>
     </div>
-  </nav>
+  </div>
+
+  <div class="col-3 text-center">
+    <div class="d-flex flex-column">
+      <a href="{{route('examination.create')}}" class="text-decoration-none">
+        <i class="fa fa-edit" style="font-size:4rem; color:black"></i>
+      
+      <h4>New examination</h4>
+    </a>
+    </div>
+  </div>
+</div>
--- resources/views/examiner/dashboard.blade.php
+++ resources/views/examiner/dashboard.blade.php
@@ -48,13 +48,13 @@
               <li class="list-group-item">
                 <div class="row">
                     <div class="col-4">
-                        <a href="{{route('enroll.students', $examination)}}" class="btn btn-sm btn-success">Enroll students</a>
+                        <a href="{{route('enroll.students', $examination)}}" class="btn btn-sm btn-secondary">Enroll students</a>
                     </div>
                     <div class="col-4">
                         <a href="" class="btn btn-sm btn-secondary">Set questions</a>
                     </div>
                     <div class="col-4">
-                        <a href="" class="btn btn-sm btn-primary">Review answers</a>
+                        <a href="" class="btn btn-sm btn-secondary">Review answers</a>
                     </div>
                 </div>
               </li>
